f050c599c14863ddd0106660835e153424b745b7
Anthonny G Machado <anthonny.gm@gmail.com>
2024-08-09 19:55:04 -0300

    feat: add router subpackage

---
+++ router/router.go
@@ -0,0 +1,13 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func Initialize() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":5000")
+}
