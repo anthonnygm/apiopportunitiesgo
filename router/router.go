813d2e927dc01eae2909118a8040cfdc46b32a0c
Anthonny G Machado <anthonny.gm@gmail.com>
2024-08-09 21:24:46 -0300

    feat: add routes opening crud without final logics

--- router/router.go
+++ router/router.go
@@ -3,11 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":5000")
+	// Initialize Router
+	router := gin.Default()
+
+	// Config Routes
+	configRoutes(router)
+
+	// Run the server
+	router.Run(":5000")
 }
